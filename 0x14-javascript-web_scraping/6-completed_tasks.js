54bbaa5af10ab75535ab76aa07dc5fc769f0303e
Kpor Kennedy <pascal.kwame@outlook.com>
2023-07-25 13:32:17 -0700

    javascript web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,10 @@
 #!/usr/bin/node
 
+// get number of tasks completed by userId
 const request = require('request');
-const url = process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
-    }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+request(process.argv[2], (error, response, body) => {
+  !error && console.log(JSON.parse(body).reduce(function (all, curr) {
+    curr.completed && (all[curr.userId] = (all[curr.userId] || 0) + 1);
+    return all;
+  }, {}));
 });
